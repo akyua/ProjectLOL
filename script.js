6ff14c8a5f2e4ddaa11a267458de8ab8f6bd03d3
Gabriel <gabrielnovatoo@gmail.com>
2022-06-21 16:39:14 -0300

    finish

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const inputs = document.querySelectorAll('.input');
-const button = document.querySelector('.login__button');
+const button = document.querySelector('.login_button');
 
 const handleFocus = ({ target }) => {
-  const span = target.previous.ElementSibling;
+  const span = target.previousElementSibling;
   span.classList.add('span-active');
 }
 
 const handleFocusOut = ({ target }) => {
-  if (target.value === ''){
+  if (target.value === '') {
     const span = target.previousElementSibling;
     span.classList.remove('span-active');
   }
@@ -16,13 +16,13 @@ const handleFocusOut = ({ target }) => {
 const handleChange = () => {
   const [username, password] = inputs;
 
-  if (username.value && password.value.length >= 8){
+  if (username.value && password.value.length >= 8) {
     button.removeAttribute('disabled');
-  } else{
+  } else {
     button.setAttribute('disabled', '');
   }
 }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
-inputs.forEach((input) => input.addEventlistener('input', handleChange));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
